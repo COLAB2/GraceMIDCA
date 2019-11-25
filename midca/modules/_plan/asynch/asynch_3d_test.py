13885797cad51e7da2ba10423023f6a6ad0c13c7
Sravya Kondrakunta <sravyachowdary1994@gmail.com>
2019-11-25 15:58:40 -0500

    Test version without realtime communication with GRACE

---
+++ midca/modules/_plan/asynch/asynch_3d_test.py
@@ -0,0 +1,337 @@
+from midca import rosrun, plans
+from midca import midcatime
+import traceback
+import math
+import copy
+import numpy as np
+
+try:
+    from geometry_msgs.msg import PointStamped
+    from geometry_msgs.msg import Point
+    from MIDCA.examples import ObjectDetector
+    from std_msgs.msg import String
+    from scipy.spatial import distance
+except:
+    pass  # if ROS is not installed, an error message will already have been generated.
+
+END_COLOR_CODE = '\033[0m'
+NOT_STARTED = 0
+NS_COLOR_CODE = END_COLOR_CODE
+IN_PROGRESS = 1
+IP_COLOR_CODE = '\033[92m'
+COMPLETE = 2
+C_COLOR_CODE = '\033[94m'
+FAILED = 3
+F_COLOR_CODE = '\033[91m'
+
+FEEDBACK_KEY = "code"
+CMD_ID_KEY = "cmd_id"
+POINT_TOPIC = "point_cmd"
+LOC_TOPIC = "loc_cmd"
+GRAB_TOPIC = "grabbing_cmd"
+RAISE_TOPIC = "raise_cmd"
+RELEASE_TOPIC = "release_cmd"
+# set this to change output for all asynch actions.
+verbose = 2
+
+MAX_SIGHTING_LAG = 3.0
+MAX_SIGHTING_WAIT = 5.0
+
+
+def get_asynch_action(midcaAction):
+    raise ArgumentException("midca action " + str(midcaAction) + " does not translate to a \
+    valid asynchronous action.")
+
+
+def asynch_plan(mem, midcaPlan):
+    '''
+    returns an asynchronous plan that corresponds to the given MIDCA plan.
+    '''
+    actions = []
+    goals = midcaPlan.goals
+    for midcaAction in midcaPlan.actions:
+        if midcaAction.op == "communicatepooldepth":
+            actions.append(GraceCommunicate(mem, midcaAction))
+
+        elif midcaAction.op == "dive":
+            actions.append(GraceDive(mem, midcaAction))
+
+        elif midcaAction.op == "raise":
+            actions.append(GraceRaise(mem, midcaAction))
+
+        elif midcaAction.op == "sensedepth":
+            actions.append(GraceSense(mem, midcaAction))
+
+        else:
+            if verbose >= 1:
+                print "MIDCA action", midcaAction, "does not correspond to an asynch",
+                "action. MIDCA will skip this action"
+    return AsynchPlan(actions, goals)
+
+
+class AsynchPlan(plans.Plan):
+    '''
+    subclass of MIDCA Plan class that uses asynchronous actions.
+    '''
+
+    def finished(self):
+        '''
+        overrides plan.finished(). Declares a plan complete if all its actions report
+        complete or failed.
+        '''
+        for action in self.actions:
+            if action.status != COMPLETE and action.status != FAILED:
+                return False
+        return True
+
+    @property
+    def status(self):
+        '''
+        property that returns the plan's status. This can be NOT_STARTED, IN_PROGRESS,
+        FAILED, or COMPLETE. If any action fails, the plan is considered to have failed.
+        The plan is complete when all actions are complete.
+        '''
+        status = COMPLETE
+        for action in self.actions:
+            if action.status == FAILED:
+                return FAILED
+            elif action.status == NOT_STARTED and status == COMPLETE:
+                status = NOT_STARTED
+            elif action.status == IN_PROGRESS:
+                status = IN_PROGRESS
+        return status
+
+    def __str__(self):
+        s = ""
+        for action in self.actions:
+            if action.status == NOT_STARTED:
+                s += NS_COLOR_CODE
+            elif action.status == IN_PROGRESS:
+                s += IP_COLOR_CODE
+            elif action.status == FAILED:
+                s += F_COLOR_CODE
+            elif action.status == COMPLETE:
+                s += C_COLOR_CODE
+
+            s += str(action) + " "
+        return s[:-1] + END_COLOR_CODE
+
+
+class AsynchAction:
+    nextID = 0
+
+    def __init__(self, mem, midcaAction, executeFunc, isComplete, blocks):
+        self.status = NOT_STARTED
+        self.mem = mem
+        self.midcaAction = midcaAction
+        self.executeFunc = executeFunc
+        self.isComplete = isComplete
+        self.blocks = blocks
+        self.startTime = None
+        self.id = AsynchAction.nextID
+        AsynchAction.nextID += 1
+
+    def execute(self):
+        if not self.startTime:
+            self.startTime = midcatime.now()
+        self.status = IN_PROGRESS
+        if not self.executeFunc:
+            return
+        try:
+            self.executeFunc(self.mem, self.midcaAction, self.status)
+        except:
+            if verbose >= 2:
+                print "Error executing action", self, ":\n", traceback.format_exc(),
+                "\n\nAction assumed to be failed"
+            self.status = FAILED
+
+    def check_complete(self):
+        if not self.startTime:
+            self.startTime = midcatime.now()
+        if not self.check_complete:
+            return
+        try:
+            complete = self.isComplete(self.mem, self.midcaAction, self.status)
+            if verbose >= 2 and not complete:
+                print "Action", self, "not complete."
+            if verbose >= 1 and complete:
+                print "Action", self, "complete."
+            if complete:
+                self.status = COMPLETE
+            return complete
+        except:
+            if verbose >= 1:
+                print "Error checking completion status for action", self, " - Assuming \
+                 failure:\n", traceback.format_exc()
+            self.status = FAILED
+
+    def ros_msg(self, topic, d):
+        '''
+        arg d should be a dictionary that contains the key/value pairs to be sent.
+        '''
+        sent = rosrun.send_msg(topic, rosrun.dict_as_msg)
+        if not sent:
+            if verbose >= 1:
+                print "Unable to send msg; ", d, "on topic", topic, " Action", self,
+                "assumed failed."
+            self.status = FAILED
+
+    def __str__(self):
+        return str(self.midcaAction)
+
+
+class GraceMidcaAct():
+    def __init__(self):
+        gracePath = '/home/pi/Desktop/Grace_Control'
+        self.bottomDepth = 0
+        self.gracePath = gracePath
+        import sys
+        sys.path.insert(0, gracePath)
+        import GliderFunIPC
+        self.interface = GliderFunIPC.GraceFun()
+
+    def communicateDepth(self, depth):  # returns boolean telling if depth was sent out over xbee
+        grace = self.interface
+        return grace.sendXbeeMsg("Depth:" + str(depth))
+
+    def dive(self):  # fills robot tank with water to make it sink
+        grace = self.interface
+        # grace.movePump(4)
+        grace.moveMass(4)
+
+    def rise(self):  # expells water from robot to make it float
+        grace = self.interface
+        # grace.movePump(98)
+        grace.moveMass(98)
+
+    def senseDepth(self):  # reads the pressure sensor and converts it to depth in meters
+        grace = self.interface
+        return grace.readDepth()
+
+    def checkCommunicationAck(self):  # read a file output by program chechinkg for surface and return true or false
+        # sending $%GO%$ over xbee will cause a file "Next_Dive_GO" to be produce with 1 in line one
+        Acknowleged = False
+        gracePath = self.gracePath + "/"
+        try:
+            f = open(gracePath + "Next_Dive_GO", 'r')
+            Acknowleged = (1 == int(f.readline()))
+            f.close()
+            if Acknowleged:
+                f = open(gracePath + "Next_Dive_GO", 'w')
+                f.write("0")
+                f.close()
+        except:
+            return False
+        return Acknowleged
+
+
+class GraceSense(AsynchAction):
+    '''
+    Grace action that senses depth
+    '''
+
+    def __init__(self, mem, midcaAction):
+        self.GraceAct = GraceMidcaAct()
+        self.mem = mem
+        self.skip = True
+        self.complete = False
+        executeAction = lambda mem, midcaAction, status: self.implement_action()
+        completionCheck = lambda mem, midcaAction, status: self.check_confirmation()
+        AsynchAction.__init__(self, mem, midcaAction, executeAction,
+                              completionCheck, True)
+
+    def implement_action(self):
+        pass
+
+    def check_confirmation(self):
+        # world = self.mem.get(self.mem.STATES)[-1]
+        if not self.skip:
+            # check if the depth is in memory
+            return True
+
+        # not usefull code for now
+        #	for atom in world.atoms:
+        #		if atom.predicate.name == "knows" or atom.predicate.name == "at_surface" or atom.predicate.name == "at_bottom":
+        #			return True
+
+        self.skip = False
+        return False
+
+
+class GraceCommunicate(AsynchAction):
+    '''
+    Action that communicates it's depth to fumin
+    '''
+
+    def __init__(self, mem, midcaAction):
+        self.GraceAct = GraceMidcaAct()
+        self.mem = mem
+        self.skip = True
+        self.depth = None
+        self.complete = False
+        executeAction = lambda mem, midcaAction, status: self.implement_action()
+        completionCheck = lambda mem, midcaAction, status: self.check_confirmation()
+        AsynchAction.__init__(self, mem, midcaAction, executeAction,
+                              completionCheck, True)
+
+    def implement_action(self):
+        pass
+
+    def check_confirmation(self):
+        world = self.mem.get(self.mem.STATES)[-1]
+        if not self.skip:
+            for atom in world.atoms:
+                if atom.predicate.name == "knows" and atom.args[0].name == "fumin":
+                    return True
+        self.skip = False
+        return False
+
+
+class GraceRaise(AsynchAction):
+    '''
+    Action to make grace reach surface
+    '''
+
+    def __init__(self, mem, midcaAction):
+        self.GraceAct = GraceMidcaAct()
+        self.mem = mem
+        self.complete = False
+        executeAction = lambda mem, midcaAction, status: self.implement_action()
+        completionCheck = lambda mem, midcaAction, status: self.check_confirmation()
+        AsynchAction.__init__(self, mem, midcaAction, executeAction,
+                              completionCheck, True)
+
+    def implement_action(self):
+        pass
+
+    def check_confirmation(self):
+        world = self.mem.get(self.mem.STATES)[-1]
+        for atom in world.atoms:
+            if atom.predicate.name == "at_surface":
+                return True
+        return False
+
+
+class GraceDive(AsynchAction):
+    '''
+    Makes grace dive
+    '''
+
+    def __init__(self, mem, midcaAction):
+        self.GraceAct = GraceMidcaAct()
+        self.mem = mem
+        self.complete = False
+        executeAction = lambda mem, midcaAction, status: self.implement_action()
+        completionCheck = lambda mem, midcaAction, status: self.check_confirmation()
+        AsynchAction.__init__(self, mem, midcaAction, executeAction,
+                              completionCheck, True)
+
+    def implement_action(self):
+        pass
+
+    def check_confirmation(self):
+        world = self.mem.get(self.mem.STATES)[-1]
+        for atom in world.atoms:
+            if atom.predicate.name == "at_bottom":
+                return True
+        return False
--- midca/modules/planning.py
+++ midca/modules/planning.py
@@ -1,7 +1,7 @@
 from _plan import pyhop
 from _plan import modified_pyhop
 from midca import plans, base
-from midca.modules._plan.asynch import asynch_3d
+from midca.modules._plan.asynch import asynch_3d, asynch_3d_test
 from midca.modules._plan.jShop import JSHOP, JSHOP2
 from midca.modules._plan.pyhop import print_state,  print_methods, print_operators
 import collections
@@ -465,6 +465,156 @@ class JSHOPPlanner(base.BaseModule):
             #change from jshop plan to MIDCA plan
             midcaPlan = plans.Plan([plans.Action(action[0], *list(action[1:])) for action in jshopPlan], goals)
             asynchPlan = asynch_3d.asynch_plan(self.mem, midcaPlan)
+
+            if verbose >= 1:
+                print "Planning complete."
+            if verbose >= 2:
+                print "Plan: "#, midcaPlan
+                for a in midcaPlan:
+                    print("  "+str(a))
+            #save new plan
+            # save new plan
+            if asynchPlan != None:
+                self.mem.get(self.mem.GOAL_GRAPH).addPlan(asynchPlan)
+            if trace: trace.add_data("PLAN",midcaPlan)
+
+class JSHOPPlannerTest(base.BaseModule):
+    '''
+    MIDCA module that implements a python version of the SHOP hierarchical task network (HTN) planner. HTN planners require a set of user-defined methods to generate plans; these are defined in the methods python module and declared in the constructor for this class.
+    Note that this module uses has several methods to translate between MIDCA's world and goal representations and those used by pyhop; these should be changed if a new domain is introduced.
+    '''
+
+    jshop_state_from_world = None
+    jshop_tasks_from_goals = None
+    domain_file = ""
+    state_file = ""
+
+    def __init__(self,
+                 jshop_state_from_world,
+                 jshop_tasks_from_goals,
+                 domain_file,
+                 state_file,
+                 extinguishers = False,
+                 mortar = False):
+
+        self.jshop_state_from_world = jshop_state_from_world
+        self.jshop_tasks_from_goals = jshop_tasks_from_goals
+        self.domain_file = domain_file
+        self.state_file= state_file
+
+        self.validate_plan = lambda plan: asynch_3d.FAILED not in [action.status for action in plan]
+
+        try:
+            self.working = True
+        except:
+            print "Error declaring pyhop methods and operators. This planner will be \
+            disabled"
+            traceback.print_exc()
+            self.working = False
+
+    def get_old_plan(self, goals, verbose = 2):
+        try:
+            plan = self.mem.get(self.mem.GOAL_GRAPH).getMatchingPlan(goals)
+            if not plan:
+                return None
+            try:
+                if self.validate_plan:
+                    valid = self.validate_plan(plan)
+                    if valid:
+                        if verbose >= 2:
+                            print "Old plan found that tests as valid:", plan
+                    else:
+                        if verbose >= 2:
+                            print "Old plan found that tests as invalid:", plan, ". removing from stored plans."
+                        self.mem.get(self.mem.GOAL_GRAPH).removePlan(plan)
+                else:
+                    if verbose >= 2:
+                        print "no validity check specified. assuming old plan is valid."
+                        valid = True
+            except:
+                if verbose >= 2:
+                    print "Error validating plan:", plan
+                valid = False
+        except:
+            print "Error checking for old plans"
+            plan = None
+            valid = False
+        if valid:
+            return plan
+        return None
+
+    def init(self, world, mem):
+        self.world = world
+        self.mem = mem
+        self.mem.set(self.mem.PLANNING_COUNT, 0)
+
+
+    #this will require a lot more error handling, but ignoring now for debugging.
+    def run(self, cycle, verbose = 2):
+        world = self.mem.get(self.mem.STATES)[-1]
+        try:
+            goals = self.mem.get(self.mem.CURRENT_GOALS)[-1]
+        except:
+            goals = []
+
+        trace = self.mem.trace
+        if trace:
+            trace.add_module(cycle,self.__class__.__name__)
+            trace.add_data("WORLD", copy.deepcopy(world))
+            trace.add_data("GOALS", copy.deepcopy(goals))
+
+        if not goals:
+            if verbose >= 2:
+                print "No goals received by planner. Skipping planning."
+            return
+        try:
+            midcaPlan = self.mem.get(self.mem.GOAL_GRAPH).getMatchingPlan(goals)
+        except AttributeError:
+            midcaPlan = None
+        if midcaPlan:
+            if verbose >= 2:
+                print "Old plan retrieved. Checking validity...",
+            valid = self.get_old_plan(goals)
+            if not valid:
+                midcaPlan = None
+                #if plan modification is added to MIDCA, do it here.
+                if verbose >= 2:
+                    print "invalid."
+            elif verbose >= 2:
+                print "valid."
+
+        #ensure goals is a collection to simplify things later.
+        if not isinstance(goals, collections.Iterable):
+            goals = [goals]
+
+        if not midcaPlan:
+            #use jshop to generate new plan
+            if verbose >= 2:
+                print "Planning..."
+            try:
+                jshopState = self.jshop_state_from_world(world, self.state_file)
+            except Exception:
+                print "Could not generate a valid jshop state from current world state. Skipping planning"
+            try:
+                jshopTasks = self.jshop_tasks_from_goals(goals,jshopState, self.state_file)
+            except Exception:
+                print "Could not generate a valid jshop task from current goal set. Skipping planning"
+            try:
+                self.mem.set(self.mem.PLANNING_COUNT, 1+self.mem.get(self.mem.PLANNING_COUNT))
+                jshopPlan = JSHOP.jshop(jshopTasks, self.domain_file, self.state_file)
+            except Exception:
+                jshopPlan = None
+            if not jshopPlan and jshopPlan != []:
+                if verbose >= 1:
+                    print "Planning failed for ",
+                    for goal in goals:
+                        print goal, " ",
+                    print
+                if trace: trace.add_data("PLAN", jshopPlan)
+                return
+            #change from jshop plan to MIDCA plan
+            midcaPlan = plans.Plan([plans.Action(action[0], *list(action[1:])) for action in jshopPlan], goals)
+            asynchPlan = asynch_3d_test.asynch_plan(self.mem, midcaPlan)
 
             if verbose >= 1:
                 print "Planning complete."
